bb22bc41b4a988403c31796aacbc0821a5c7f8cc
@TanujMittal12 <mtanuj121@gmail.com>
2026-08-19 01:40:26 +0530

    frontend-url-change

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app=express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://interview-ai-nine-gules.vercel.app",
   credentials:true
 }))
 
